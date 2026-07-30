08d2df2f2105f4710c680a5dae6d5df24e5b6151
wad-sh <tawjehiwadee@gmail.com>
2026-07-31 02:26:27 +0300

    added create admin script

---
+++ app/scripts/create_admin.py
@@ -0,0 +1,36 @@
+from database.database import SessionLocal
+from models.user import User
+from auth.security import hash_pw
+from enums.adminuser import AdminUser
+from getpass import getpass
+
+db = SessionLocal()
+try :
+    usernameA = input("Enter username: ")
+    emailA = input("Enter email: ")
+    passwordA = getpass("Enter password: ")
+
+    ex_un = db.query(User).filter(User.username == usernameA).first()
+    ex_em = db.query(User).filter(User.email == emailA).first()
+    if ex_un is not None:
+        raise ValueError("Username exists already")
+    if ex_em is not None:
+        raise ValueError("Email exists already")
+    
+    admin = User(
+        username = usernameA,
+        email = emailA,
+        hashed_password = hash_pw(passwordA),
+        role = AdminUser.admin
+    )
+
+    db.add(admin)
+    db.commit()
+    db.refresh(admin)
+
+    print(admin)
+except Exception as e:
+    db.rollback()
+    print(e)
+finally:
+    db.close()
